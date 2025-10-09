26e39810c23d7c745ed6c90f45d0d101593329b2
Kirteous <grewalkirt0815@gmail.com>
2025-10-08 21:07:20 -0700

    lab 1 finished

--- camera_part_one.py
+++ camera_part_one.py
@@ -1,29 +1,30 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)	
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-while(1):
- 
-    # Take each frame
-    _, frame = cap.read()
  
     # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
      # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_level = np.array([110,50,50])
+    upper_level = np.array([135,255,255])
     
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    # Threshold the HSV image to get only pink colors
+    mask = cv2.inRange(hsv, lower_level, upper_level)
  
-    cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
-    cv.imshow('res',res)
+    contours0, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if contours0:
+        c = max(contours0, key=cv2.contourArea)
+        if cv2.contourArea(c) > 1000:
+            x,y,w,h = cv2.boundingRect(c)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- camera_part_two.py
+++ camera_part_two.py
@@ -1,16 +1,35 @@
 import numpy as np
 import cv2
+from sklearn.cluster import KMeans
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    img_height,img_width,_ = frame.shape
+    track_height = img_height//4
+    track_width = img_width//4
+    xl = img_width//2 - track_width//2
+    xr = xl + track_width
+    yb = img_height//2 - track_height//2
+    yt = yb + track_height
+
+    region_of_interest = frame[yb:yt, xl:xr]
+    array = region_of_interest.reshape(-1,3)
     
+    kmeans = KMeans(n_clusters=1, n_init="auto", random_state=42)
+    kmeans.fit(array)
+    dom_color = kmeans.cluster_centers_[0].astype(np.uint8)
+
+    cv2.rectangle(frame, (xl, yb), (xr, yt), (0, 255, 0), 2)
+    template = np.zeros((100,100,3), dtype = np.uint8)
+    template[:] = dom_color
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', frame)
+    cv2.imshow("Dominant Color", template)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
